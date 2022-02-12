841d061d5f099391c21277672bf42a2634dd0da0
FanisNg <fanisnigamad@yandex.ru>
2022-02-12 17:43:05 +0300

    4th task

---
+++ Lab_1.1(DPSA)/Lab_1.1(DPSA).cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+
+struct Node
+{
+    int data;
+    Node* link;
+};
+
+Node* top = NULL;
+Node* additionallyTop = NULL;
+char choice;
+
+bool addIsempty()
+{
+    if (additionallyTop == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool isempty()
+{
+    if (top == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void push(int value)
+{
+    std::cout << "1-добавить новый элемент\n"
+        "2-добавить элемент из вспомогательного стека" << std::endl;
+
+    while (true)
+    {
+        while ((!(std::cin >> choice) || (std::cin.peek() != '\n')))
+        {
+            std::cin.clear();
+            while (std::cin.get() != '\n');
+            std::cout << "ошибка!" << std::endl;
+        }
+        if (choice == '1')
+        {
+            Node* ptr = new Node;
+            ptr->data = value;
+            ptr->link = top;
+            top = ptr;
+            break;
+        }
+        else if (choice == '2')
+        {
+            if (addIsempty())
+            {
+                std::cout << "Вспомогательный стек пуст. Добавить нечего" << std::endl;
+                break;
+            }
+            else
+            {
+                Node* ptr = additionallyTop;
+                additionallyTop = additionallyTop->link;
+                ptr->link = top;
+                top = ptr;                
+                break;
+            }
+
+        }
+        else
+        {
+            std::cout << "Вы ввели что-то не из представленного списка!" << std::endl;
+        }
+    }
+}
+
+void pop()
+{
+    if (isempty())
+    {
+        std::cout << "Stack is already empty" << std::endl;
+    }
+    else
+    {
+        std::cout << "1-полностью удалить элемент\n"
+        "2-извлечь из текущего стека и добавить во вспомогательный стек" << std::endl;
+
+        while (true)
+        {
+            while ((!(std::cin >> choice) || (std::cin.peek() != '\n')))
+            {
+                std::cin.clear();
+                while (std::cin.get() != '\n');
+                std::cout << "ошибка!" << std::endl;
+            }
+            if (choice == '1')
+            {
+                Node* ptr = top;
+                top = top->link;
+                delete(ptr);
+                break;
+            }
+            else if (choice == '2')
+            {
+                Node* ptr = top;
+                top = top->link;                
+                ptr->link = additionallyTop;
+                additionallyTop = ptr;
+                break;
+            }
+            else
+            {
+                std::cout << "Вы ввели что-то не из представленного списка!" << std::endl;
+            }
+        }
+        
+        
+    }
+}
+
+void showTop()
+{
+    if (isempty())
+    {
+        std::cout << "Stack is already empty" << std::endl;
+    }
+    else
+    {
+        std::cout << "The top is " << top->data;
+    }
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    push(19);
+    push(20);
+    push(1001);
+    pop();
+    pop();
+    push(12);
+    
+
+    return 0;
+}
+
